abacf75b491d25145d8d5909ab156b3b51919feb
zinminnhtun <zinmintoun@gmail.com>
2025-05-28 20:15:36 +0700

    restart

--- lib/core/models/lesson.dart
+++
@@ -1,8 +0,0 @@
-class Lesson {
-  final String id;
-  final String title;
-  final String subtitle; // e.g. “Phrases you need”
-  // you’ll expand this later—e.g. List<Word>, audio URLs…
-
-  Lesson({required this.id, required this.title, required this.subtitle});
-}
